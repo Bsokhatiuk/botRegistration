e3a02238d461b903c4820e3ef6f7a5791883cf66
bsokhatiuk <b.sohatuyk@gmail.com>
2023-07-07 15:34:47 +0300

    add form to github and send data from user

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ dp = Dispatcher(bot)
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 b1 = KeyboardButton("⚒help")
 b2 = KeyboardButton("start")
-b3 = KeyboardButton("view", web_app=WebAppInfo(url="https://telemetr.io/uk/channels"))
+b3 = KeyboardButton("view", web_app=WebAppInfo(url="https://bsokhatiuk.github.io"))
 
 kb.add(b1).insert(b2).add(b3)
 
 ikb = InlineKeyboardMarkup(row_width=2)
-ib1 = InlineKeyboardButton
+ib1 = InlineKeyboardButton("site", web_app=WebAppInfo(url="https://bsokhatiuk.github.io"))
+ikb.add(ib1)
 HELP_COMMENDS = """
 <b>/start</b> - розпочати роботу бота
 <b>⚒help</b> - список команд
@@ -38,7 +39,7 @@ async def help_command(message:types.Message):
 async def start_command(message:types.Message):
     text = """ save user data """
     await dao.create_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
-    await bot.send_message(chat_id=message.from_user.id, text=text, parse_mode='HTML',reply_markup=kb)
+    await bot.send_message(chat_id=message.from_user.id, text=text, parse_mode='HTML',reply_markup=ikb)
     # await message.delete()
 
 
