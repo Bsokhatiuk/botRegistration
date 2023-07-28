be9cf3ba913a388f26deab1805fb9da80220a6fa
bsokhatiuk <b.sohatuyk@gmail.com>
2023-07-28 11:49:32 +0300

    add sservice func

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def calendar(id):
 def req(id):
     data = dao.get_employee_all()
     response = app.response_class(
-        response=json.dumps({'result':'fin'}),
+        response=json.dumps({'data':data}),
         status=200,
         mimetype='application/json'
         )
